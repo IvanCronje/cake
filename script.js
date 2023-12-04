cfe3e43d648a21866724bc4745a912bb6057e886
IvanCronje <142512088+IvanCronje@users.noreply.github.com>
2023-12-04 18:32:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const candle = document.getElementById("candle");
-
+  
     candle.addEventListener("click", function () {
       candle.classList.add("extinguished");
     });
-
-    // Update the text to display
-    const birthdayMessage = document.querySelector('.birthday-message p');
-    birthdayMessage.textContent = "Happy Birthday!";
-
-   
-});
+  });
+  
